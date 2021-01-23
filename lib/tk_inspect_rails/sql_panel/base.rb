6088142cff7557a3e98a21e376d21fadf8c3d0e2
Josep Egea <jes@josepegea.com>
2021-01-23 08:41:19 +0100

    Added key accelerator to execute SQL

--- lib/tk_inspect_rails/sql_panel/base.rb
+++ lib/tk_inspect_rails/sql_panel/base.rb
@@ -18,10 +18,16 @@ module TkInspectRails
         @main_component = RootComponent.new
         @main_component.sql_panel = self
         @tk_root.place_root_component(@main_component)
+        create_menu
       end
 
       def execute_sql(expression)
         ActiveRecord::Base.connection.execute(expression)
+      end
+
+      # So far, we only register key accelerators. Menus are better inherited from main console
+      def create_menu
+        @tk_root.tk_item.native_item.bind('Command-r', -> { main_component.execute_sql(nil) })
       end
     end
   end
