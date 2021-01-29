27d19ff329f6c6fa0bbababdb2b181b6d025081f
Josep Egea <jes@josepegea.com>
2021-01-29 21:25:51 +0100

    Refactored to use render instead of generate

--- lib/tk_inspect_rails/sql_panel/root_component.rb
+++ lib/tk_inspect_rails/sql_panel/root_component.rb
@@ -5,23 +5,21 @@ module TkInspectRails
       attr_accessor :expression
       attr_accessor :results
 
-      def generate(parent_component, options = {})
-        parse_component(parent_component, options) do |p|
-          p.vpaned(sticky: 'wens', x_flex: 1, y_flex: 1) do |f|
-            f.vframe(sticky: 'wens', x_flex: 1) do |vf|
-              vf.hframe(sticky: 'wen', padding: 8, x_flex: 1) do |hf|
-                hf.label(text: "SQL Expression", sticky: 'w', x_flex: 1)
-                hf.button(text: "Execute", sticky: 'e', on_click: :execute_sql)
-              end
-              @text = vf.text(value: @expression.to_s, width: 20, height: 5, scrollers: 'y',
-                              x_flex: 1, y_flex: 1, sticky: 'news')
+      def render(p, parent_component)
+        p.vpaned(sticky: 'wens', x_flex: 1, y_flex: 1) do |f|
+          f.vframe(sticky: 'wens', x_flex: 1) do |vf|
+            vf.hframe(sticky: 'wen', padding: 8, x_flex: 1) do |hf|
+              hf.label(text: "SQL Expression", sticky: 'w', x_flex: 1)
+              hf.button(text: "Execute", sticky: 'e', on_click: :execute_sql)
             end
-            f.insert_component(TkComponent::TableViewComponent, self,
-                               data_source: self,
-                               columns: @results&.first&.keys&.map { |k| { key: k, text: k } } || [],
-                               scrollers: 'xy',
-                               sticky: 'nsew', x_flex: 1, y_flex: 1)
+            @text = vf.text(value: @expression.to_s, width: 20, height: 5, scrollers: 'y',
+                            x_flex: 1, y_flex: 1, sticky: 'news')
           end
+          f.insert_component(TkComponent::TableViewComponent, self,
+                             data_source: self,
+                             columns: @results&.first&.keys&.map { |k| { key: k, text: k } } || [],
+                             scrollers: 'xy',
+                             sticky: 'nsew', x_flex: 1, y_flex: 1)
         end
       end
 
